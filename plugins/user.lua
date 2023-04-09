199153f76166995522553be622505ee803a0860d
MilhoNerfado <emilio.bottoni@edge.ufal.br>
2023-04-09 03:25:50 -0300

    Add Harpoon!! Blazing FAST!

--- plugins/user.lua
+++ plugins/user.lua
@@ -20,4 +20,18 @@ return {
       { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
     },
   },
+  {
+    "ThePrimeagen/harpoon",
+    dependencies = "nvim-lua/plenary.nvim",
+    opts = {},
+    keys = {
+      { "<A-'>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Toggle harpoon quick menu" },
+      { '<A-">', "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Add a file to harpoon" },
+      { "<A-1>", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Jumps to file 1 in harpoon" },
+      { "<A-2>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Jumps to file 2 in harpoon" },
+      { "<A-3>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Jumps to file 3 in harpoon" },
+      { "<A-4>", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Jumps to file 4 in harpoon" },
+      { "<A-5>", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", desc = "Jumps to file 5 in harpoon" },
+    },
+  },
 }
